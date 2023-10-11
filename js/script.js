05e9bfbee01cbb6685df473afafe1bcda91dc29b
gustavo91010 <gustavo910@gmail.com>
2023-10-11 00:13:26 -0300

    feat: end game ok

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const mario_elemento= document.querySelector('.mario_css')
+const pipe_elemento= document.querySelector('.pipe_css')
+
 /*
 documetno esta escutando unm evendo, o evento do 'abaixar da tecla', quando ela for acionada, chamarar o metodo jump
 
@@ -14,4 +16,28 @@ const jump_js= ()=>{
 
     // setTimeout vai executar uma função depois de determinado tempo, o tempo ai ja esta em milisegundos, deixei 500 que é o tempo da animação de pulo do mario
 }
-document.addEventListener('keydown', jump_js)
+// um loop para verificar se o jogo acabou, ele acaba quando o tbo bater no mario, ou seja, quando o tubo chegar a 120-x da esquerda e o mario ainda nao houver pulado...
+const loop= setInterval(()=>{
+const pipe_position=pipe_elemento.offsetLeft; 
+// const marioPosition=pipe_elemento.offsetBotton; // essa nao existe... 
+// offsetLeft pega o deslocamento da esquerda...
+
+const marioPosition=Number(window.getComputedStyle(mario_elemento).bottom.replace('px',''));
+// uma outra forma de pegar as proopriedados css do elemento. na realidade, estou analizando o elemento...
+// convertendo ela para number
+
+if(pipe_position <= 120 && pipe_position> 0 && marioPosition< 90){
+// 120 é o ultimo distanciamento ates do tubo bater
+// 90px é a altura minima para o tubo passar por baixo do mario
+
+    pipe_elemento.style.animation = 'none'
+    // se for igual a 120 ele manda parar a animação do pipe_element
+    
+    pipe_elemento.style.left= `${pipe_position}px`
+    // quando o tubo bater, vai ficar junto com ele...
+}
+
+}, 10);
+
+
+document.addEventListener('keydown', jump_js)  
